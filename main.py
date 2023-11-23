4f62ad14298f6643ce976f68f5a81340f58696d1
GodlessFlanker <150722891+GodlessFlanker@users.noreply.github.com>
2023-11-23 03:25:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+import socket
+import threading
+
+import rsa
+
+public_key, private_key = rsa.newkeys(1024)
+public_partner = None
+
+
+
+
+#ip = input("Enter your Ip adress")   //this command line we use in order to make it more functional
+
+choice = input("Do you want to host (1) or to connect (2): ")
+
+if choice == "1":
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("127.0.0.1", 9999))
+    server.listen()
+
+#The other person should specify IP of you to connect
+    client, _ = server.accept()
+    client.send(public_key.save_pkcs1("PEM"))
+    public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
+elif choice == "2":
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(("127.0.0.1", 9999))
+    client.send(public_key.save_pkcs1("PEM"))
+    public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
+else:
+    exit()
+
+
+def sending_messages(c):
+    while True:
+        message = input("")
+        c.send(rsa.encrypt(message.encode(), public_partner))
+        print("You: " + message)
+
+
+def receiving_messages(c):
+    while True:
+        
+        print("Partner: " + rsa.decrypt(c.recv(1024), private_key).decode())
+
+threading.Thread(target=sending_messages, args=(client, )).start()
+threading.Thread(target=receiving_messages, args=(client, )).start()
